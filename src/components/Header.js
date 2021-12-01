732a81ef483f14fe22b98b7b08250dad20a7292d
sergey0226 <Smitukov@bk.ru>
2021-12-01 16:42:40 +0100

    change with header buttton

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 export default function Header() {
+  const navigate = useNavigate();
+  const [pageState, setPageState] = useState('home')
   return (
     <Container>
       <AppBar position="static">
@@ -13,15 +15,9 @@ export default function Header() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             News
           </Typography>
-            <Button color="inherit">
-              <NavLink to="/">Home</NavLink>
-            </Button>
-            <Button color="inherit">
-                <NavLink to="/universities">Universities</NavLink>
-            </Button>
-            <Button color="inherit">
-                <NavLink to="/postallookup">Postal LookUp</NavLink>
-            </Button>
+            <Button color={pageState==='home'?"secondary":"inherit"} onClick={()=>{navigate('/posts'); setPageState('home')}}>Home</Button>
+            <Button color={pageState==='universities'?"secondary":"inherit"} onClick={()=>{navigate('/universities'); setPageState('universities')}}>Universities</Button>
+            <Button color={pageState==='postallookup'?"secondary":"inherit"} onClick={()=>{navigate('/postallookup'); setPageState('postallookup')}}>Postal LookUp</Button>
         </Toolbar>
       </AppBar>
     </Container>
